9be2a646f1887e2caf073ca79c31363a5c864e2a
권도현 <ehgus8313@naver.com>
2019-09-16 16:32:01 +0900

    PlayStore_Launching_Ver1.1.1 버그수정 Edittext 천단위기호 표시 추가 판매 전 금액 확인칸 추가

--- app/src/main/java/com/kwon/ehgus/fo4commission/MainActivity.java
+++ app/src/main/java/com/kwon/ehgus/fo4commission/MainActivity.java
@@ -1,7 +1,10 @@
 package com.kwon.ehgus.fo4commission;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,18 +13,22 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import java.text.DecimalFormat;
+
 import static android.view.View.GONE;
 import static android.view.View.VISIBLE;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    private EditText Saleamount, Fee, Self, Discount, Receive;
+    private EditText Saleamount, Fee, Self, Discount, Receive,origin;
     private Button Button1, Button2;
     private RadioGroup radioGroup;
     private double feeamount=0;
     private RadioButton fee_radio1, fee_radio2, fee_radio3, fee_radio4 ,fee_radio5, fee_radio6, fee_radio7;
     private LinearLayout lay;
+    private DecimalFormat format = new DecimalFormat("###,###");//콤마
+    private String Editresult="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +36,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Saleamount = (EditText) findViewById(R.id.Saleamount);
+        Saleamount.addTextChangedListener(watcher);
         Fee = (EditText) findViewById(R.id.Fee);
         Self = (EditText) findViewById(R.id.Self);
         Discount = (EditText) findViewById(R.id.Discount);
         Receive = (EditText) findViewById(R.id.Receive);
+        origin = (EditText) findViewById(R.id.origin);
 
 
         //라디오 버튼 설정
@@ -60,17 +69,28 @@ public class MainActivity extends AppCompatActivity {
         Button1.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String tmp1 = Saleamount.getText().toString();
+                String tmp1 = Saleamount.getText().toString().replaceAll("\\,","");;
                 if (Saleamount.length() <= 0) {
                     Toast.makeText(MainActivity.this, "금액을 입력해주세요.", Toast.LENGTH_SHORT).show();
                 } else {
                     //판매금액 받기
+                    Integer original = (int) (Integer.parseInt(tmp1) - (Integer.parseInt(tmp1) * 0.3));
                     Integer result = (int) (Integer.parseInt(tmp1) * 0.3);
                     Integer tmp3 = (int) ((Integer.parseInt(tmp1) - (Integer.parseInt(tmp1) * 0.3)) + (Integer.parseInt(tmp1) * 0.3) * feeamount);
                     Integer tmp4 = (int) ((Integer.parseInt(tmp1) * 0.3) * feeamount);
-                    Fee.setText(result.toString());
-                    Receive.setText(tmp3.toString());
-                    Discount.setText("+" + tmp4.toString());
+
+                    //천단위 기호 변환환
+                    long resultlong = Long.parseLong(String.valueOf(result));
+                    long tmp3long = Long.parseLong(String.valueOf(tmp3));
+                    long tmp4long = Long.parseLong(String.valueOf(tmp4));
+                    long originallong = Long.parseLong(String.valueOf(original));
+
+
+
+                    Fee.setText(format.format(resultlong));
+                    Receive.setText(format.format(tmp3long));
+                    Discount.setText("+" + format.format(tmp4long));
+                    origin.setText(format.format(originallong));
                     System.out.println(feeamount);
                 }
             }
@@ -93,6 +113,30 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+//Edittext입력시 천단위기호 변환
+    TextWatcher watcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            if(!TextUtils.isEmpty(charSequence.toString()) && !charSequence.toString().equals(Editresult)){
+                Editresult = format.format(Double.parseDouble(charSequence.toString().replaceAll(",","")));
+                Saleamount.setText(Editresult);
+                Saleamount.setSelection(Editresult.length());
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
+    };
+
+
+
     //라디오 버튼 클릭 리스너
     RadioButton.OnClickListener radioButtonClickListener = new RadioButton.OnClickListener() {
         @Override
